e4ca0c509c346c339cc999a196e95c429b1ef8f8
ketikai <ketikai@idealstate.team>
2025-05-11 14:40:38 +0800

    feat(main): 继承自 Sugar 接口以满足运行时自动依赖下载的触发条件
    [skip ci]
    
    Signed-off-by: ketikai <ketikai@idealstate.team>

--- spigot/src/main/java/team/idealstate/minecraft/next/spigot/MinecraftNext.java
+++ spigot/src/main/java/team/idealstate/minecraft/next/spigot/MinecraftNext.java
@@ -19,6 +19,7 @@
 package team.idealstate.minecraft.next.spigot;
 
 import team.idealstate.minecraft.next.spigot.api.SpigotPlugin;
+import team.idealstate.sugar.Sugar;
 import team.idealstate.sugar.next.boot.hikaricp.annotation.EnableHikariCP;
 import team.idealstate.sugar.next.boot.jedis.annotation.EnableJedis;
 import team.idealstate.sugar.next.boot.mybatis.annotation.EnableMyBatis;
@@ -28,7 +29,7 @@ import team.idealstate.sugar.validate.annotation.NotNull;
 @EnableHikariCP
 @EnableJedis
 @EnableMyBatis
-public final class MinecraftNext extends SpigotPlugin {
+public final class MinecraftNext extends SpigotPlugin implements Sugar {
     @Override
     public void onInitialize(@NotNull Context context) {}
 
